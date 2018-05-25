5848b8a531b0970c59b50b0b133f0f4bdbbf1af1
Luqiang Mao <luqiangmao@Luqiangs-MacBook-Pro.local>
2018-05-24 21:23:30 -0700

    forth upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var apiroutes = require("/Users/luqiangmao/FriendsFinder/app/routing/apiRoutes.j
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3002;
+var PORT = process.env.PORT || 3002;
 //var PORT = process.env.PORT || 3000
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
